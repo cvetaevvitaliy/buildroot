9f4503ff28506d5eea681b7dc26f0055b5ff8ff8
Vitaliy <vitaliy.nimych@gmail.com>
2022-03-09 13:16:48 +0200

    fix build weston, sdl2 for stm32mp1

---
+++ local.mk
@@ -0,0 +1,3 @@
+LINUX_OVERRIDE_SRCDIR = $(TOPDIR)/../buildroot-external-st/linux
+UBOOT_OVERRIDE_SRCDIR = $(TOPDIR)/../buildroot-external-st/uboot
+ARM_TRUSTED_FIRMWARE_OVERRIDE_SRCDIR = $(TOPDIR)/../buildroot-external-st/atf
--- package/libxkbcommon/libxkbcommon.mk
+++ package/libxkbcommon/libxkbcommon.mk
@@ -13,14 +13,15 @@ LIBXKBCOMMON_INSTALL_STAGING = YES
 LIBXKBCOMMON_DEPENDENCIES = host-bison host-flex
 LIBXKBCOMMON_CONF_OPTS = \
 	-Denable-docs=false \
-	-Denable-wayland=false \
-	-Denable-xkbregistry=false
+	-Denable-wayland=true \
+	-Denable-xkbregistry=true
 
 ifeq ($(BR2_PACKAGE_XORG7),y)
 LIBXKBCOMMON_CONF_OPTS += -Denable-x11=true
 LIBXKBCOMMON_DEPENDENCIES += libxcb
 else
 LIBXKBCOMMON_CONF_OPTS += -Denable-x11=false
+LIBXKBCOMMON_CONF_OPTS += --enable-wayland
 endif
 
 $(eval $(meson-package))
